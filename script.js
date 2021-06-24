ed7d477aad9902bb9ec69bf8af3cb1736ddd2e29
notmeh1 <zleckzxd@gmail.com>
2021-06-24 00:35:46 -0400

    base files

---
+++ script.js
@@ -0,0 +1,11 @@
+    // constantes para definir los números
+    const number1 = parseInt(prompt('Inserte el primer número'));
+    const number2 = parseInt(prompt('Inserte el segundo número'));
+    // constante que suma los números definidos
+    const result = number1 + number2
+    // mostrar el resultado de los números
+    document.write(`La suma entre ${number1} y ${number2} es ${result}`)
+    
+    // mostrar el resultado de los números en la consola
+    // console.log (`La suma entre ${number1} y ${number2} es ${result}`)
+    
